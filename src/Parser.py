f0fd42fae5f4a52de8a3c8f3402225a88a4ed77a
RuntimeException420 <jonashess@tuta.com>
2024-05-24 11:39:31 +0200

    Handles /assets/VeryBad.docm and writes a .json file to the working directory with the content of each found .xml and .rels file. Still open: .bin files

--- src/Parser.py
+++ src/Parser.py
@@ -6,36 +6,48 @@ import json
 
 def create_json(folder_path):
     data = {}
-    for root, d_names, f_names in os.walk(folder_path):
-        for f in f_names:
-            print(os.path.join(root, f))
-        #for f in f_names:
-         #   fname = os.path.join(root, f)
-          #  if fname.endswith(".xml") or fname.endswith(".rels"):
-           #     with open(fname, encoding="utf8") as temp:
-            #        data[fname] = temp.read()
 
-    #print(json.dumps(data, indent=2))
+    for root, d_names, f_names in os.walk(folder_path):  # walks recursively through the file system
+        path = root.replace("..\\assets\\temp_extraction\\", "")
+        parts = path.split("\\")
+        curr_dict = data
+        for i, part in enumerate(parts):
+            if part == "":
+                part = ".\\"
+            if i == len(parts):
+                curr_dict[part] = ""
+            else:
+                if part not in curr_dict:
+                    curr_dict[part] = {k: read_file_content(root, k) for k in f_names}
+                curr_dict = curr_dict[part]
+    return data
+
+
+def read_file_content(path, file):
+    file = os.path.join(path, file)
+    if file.endswith(".xml") or file.endswith(".rels"):
+        with open(file, encoding="utf8") as f:
+            return f.read()
+    return ""
 
 
 if __name__ == '__main__':
     try:
-        """
         # Duplicate Office-Document and convert it into a zip-file
         shutil.copy("../assets/VeryBad.docm", "../assets/VeryBad.zip")
-        
-        # Decompress archive
+
         zip_copy = zipfile.ZipFile("../assets/VeryBad.zip", "r")
         zip_copy.extractall("../assets/temp_extraction")
         zip_copy.close()
-        
+
         os.remove("../assets/VeryBad.zip")
-        """
-        #create_json("..\\assets\\temp_extraction\\")
-        with open("../assets/temp_extraction/word/vbaProject.bin/PROJECT", "rb") as f:
-            print(f.read())
-        print("done")
-    #except FileNotFoundError:
-    #    print("File was not found.")
+        json_dict = create_json("..\\assets\\temp_extraction\\")
+
+        with open("extracted_information.json", "w", encoding="utf-8") as output:
+            json.dump(json_dict, output, indent=4)
+
+        shutil.rmtree("../assets/temp_extraction")
+    except FileNotFoundError:
+        print("File was not found.")
     except shutil.SameFileError:
         print("Error occurred.")
