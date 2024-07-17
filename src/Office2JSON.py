2299f06fe30a57529c753cf9c37746eabaf3e40b
RuntimeException420 <jonashess@tuta.com>
2024-07-17 20:37:29 +0200

    Renamed main script from "Parser" to "Office2JSON". Outsourced main part into its own function. Added console output with "extraction time" and "extraction path"

--- src/Office2JSON.py
+++ src/Office2JSON.py
@@ -4,6 +4,8 @@ import os
 import argparse
 import subprocess
 import json
+import time
+import math
 
 
 def create_json(folder_path):
@@ -43,29 +45,37 @@ def read_file_content(path, file_name):
     return ""
 
 
+def extract(file_path, output_path: str = None):
+    if output_path is None:
+        output_path = file_path
+
+    rel_path, file = os.path.split(file_path.file)
+    rel_path += "\\"
+    temp_name = os.path.splitext(file)[0] + ".zip"
+
+    # Duplicate Office-Document and convert it into a zip-file
+    shutil.copy(file_path.file, rel_path + temp_name)
+
+    zip_copy = zipfile.ZipFile(rel_path + temp_name, "r")
+    zip_copy.extractall(rel_path + "temp_extraction")
+    zip_copy.close()
+
+    os.remove(rel_path + temp_name)
+    json_dict = create_json(rel_path + "temp_extraction")
+    with open(os.path.join(rel_path, "extracted_" + file + ".json"), "w", encoding="utf-8") as res_file:
+        json.dump(json_dict, res_file, indent=4)
+
+
 if __name__ == '__main__':
+    start_time = time.time()
     rel_path = ".\\"
     try:
-        args_parser = argparse.ArgumentParser("Parser.py")
+        args_parser = argparse.ArgumentParser("Office2JSON.py")
         args_parser.add_argument("file", help="Provide the file path to a Microsoft Office document to be analysed.")
         args = args_parser.parse_args()
 
-        rel_path, file = os.path.split(args.file)
-        rel_path += "\\"
-        temp_name = os.path.splitext(file)[0] + ".zip"
-
-        # Duplicate Office-Document and convert it into a zip-file
-        shutil.copy(args.file, rel_path + temp_name)
-
-        zip_copy = zipfile.ZipFile(rel_path + temp_name, "r")
-        zip_copy.extractall(rel_path + "temp_extraction")
-        zip_copy.close()
-
-        os.remove(rel_path + temp_name)
-        json_dict = create_json(rel_path + "temp_extraction")
-
-        with open("extracted_information.json", "w", encoding="utf-8") as res_file:
-            json.dump(json_dict, res_file, indent=4)
+        rel_path, _ = os.path.split(args.file)
+        extract(args)
 
     except FileNotFoundError as e:
         print("File was not found.\n" + e)
@@ -74,6 +84,13 @@ if __name__ == '__main__':
     except zipfile.BadZipfile as e:
         print("Can't open file as archive.\n")
     finally:
-        file = os.path.join(rel_path + "temp_extraction")
+        # clean-up
+        file = os.path.join(rel_path, "temp_extraction")
         if os.path.exists(file):
-            shutil.rmtree(rel_path + "temp_extraction")
+            shutil.rmtree(file)
+
+        print(40 * "_")
+        print(f"Extraction time: \t{round(time.time() - start_time, 3)} seconds")
+        print(f"Extraction path: \t{rel_path + '/'}")
+        print(40 * "_")
+
